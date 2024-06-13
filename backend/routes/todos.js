30e581fa660d69a62ee4c6e78d06d936e0d45722
Yogita Salve <yogitasalve99@gmail.com>
2024-06-13 17:22:04 +0530

    added code

---
+++ backend/routes/todos.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const router = express.Router();
+const Todo = require('../models/Todo');
+
+// Get all todos
+router.get('/', async (req, res) => {
+  const todos = await Todo.find();
+  res.json(todos);
+});
+
+// Create a new todo
+router.post('/', async (req, res) => {
+  const newTodo = new Todo({
+    text: req.body.text
+  });
+  const todo = await newTodo.save();
+  res.json(todo);
+});
+
+// Update a todo
+router.put('/:id', async (req, res) => {
+  const updatedTodo = await Todo.findByIdAndUpdate(req.params.id, req.body, { new: true });
+  res.json(updatedTodo);
+});
+
+// Delete a todo
+router.delete('/:id', async (req, res) => {
+  await Todo.findByIdAndDelete(req.params.id);
+  res.json({ message: 'Todo deleted' });
+});
+
+module.exports = router;
---
+++ backend/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const todoRoutes = require('./routes/todos');
+
+const app = express();
+require('dotenv').config()
+const PORT = 5000;
+
+mongoose.connect(process.env.MONGO_URI, {
+  dbName:'todos',
+  useNewUrlParser: true, 
+  useUnifiedTopology: true 
+});
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/todos', todoRoutes);
+app.use('/users', todoRoutes);
+
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
