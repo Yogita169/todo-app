d04bf0c395579bbb4d91a81ebe15e4ce3f47556b
Yogita Salve <yogitasalve99@gmail.com>
2024-07-09 18:34:52 +0530

    updates

--- backend/routes/todos.js
+++ backend/routes/todos.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Todo = require('../models/Todo');
+const Todo = require('../models/ToDo');
 
 // Get all todos
 router.get('/', async (req, res) => {
